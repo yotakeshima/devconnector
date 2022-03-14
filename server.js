b40532bc584af55a311f8a873d5e2bbe7af34cc6
yoshi <yotakeshima@gmail.com>
2022-03-14 17:00:53 -0400

    added connectDB function

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
