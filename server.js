f1081b02d09dc2e3cd1665bc884f1ffb57f8a441
Yoshi <yotakeshima@gmail.com>
2022-09-26 11:15:10 -0400

    added comments to server.js

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const app = express();
 // Connect Database
 connectDB();
 
+// 
 app.get('/', (req, res) => res.send('API Running'));
 
+// Looks for an env variable called PORT. When we deploy to heroku, it will receive a PORT. Otherwise default port is 5000.
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
