01fd4fd999d2d632768f9f604b9c0b0d20604f8e
Yoshinori Takeshima <yotakeshima@gmail.com>
2022-10-09 21:23:23 -0400

    added get all profiles and get a singular profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -102,7 +102,41 @@ router.post('/', [ auth, [
         console.error(err.message);
         res.status(500).send('Server Error');
     }
-})
+});
 
+// @route   GET api/profile/
+// @desc    Get all profiles
+// @access  Public (or private: You need to send a token to that route, in order for it to work.)
+
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+        res.json(profiles);
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public (or private: You need to send a token to that route, in order for it to work.)
+
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        // Takes the user_id as a path.
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+        if(!profile) return res.status(400).json({ msg: 'Profile not found' });
+        res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind == 'ObjectId'){
+            return res.status(400).json({ msg: 'Profile not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
 
 module.exports = router;
