55d503763593373e0f2d9926405eaba8d0957879
Yoshinori Takeshima <yotakeshima@gmail.com>
2022-12-06 15:32:18 -0500

    Revert "Revert "not working""
    This reverts commit 46bd6586f28e5a75511c9930f78a50ec6dd8d857.

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,6 @@ import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
 import "./App.css";
 
 const App = () => (
@@ -11,12 +9,6 @@ const App = () => (
     <Fragment>
       <Navbar />
       <Route exact path="/" component={Landing} />
-      <section className="container">
-        <Switch>
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/login" component={Login} />
-        </Switch>
-      </section>
     </Fragment>
   </Router>
 );
