f1b3612779334d111cf472d6e58b05d4d5e94bd5
Yoshinori Takeshima <yotakeshima@gmail.com>
2022-12-06 15:49:35 -0500

    reverted stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,25 @@
-import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Route exact path="/" component={Landing} />
-    </Fragment>
-  </Router>
-);
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -84,6 +84,7 @@ router.get("/:id", auth, async (req, res) => {
 
 router.delete("/:id", auth, async (req, res) => {
   try {
+    //-1 finds the most recent date
     const post = await Post.findById(req.params.id);
 
     //Check if the user deleting the post is the owner
@@ -101,132 +102,6 @@ router.delete("/:id", auth, async (req, res) => {
     if (err.kind === "ObjectId") {
       return res.status(404).json({ msg: "Post not found" });
     }
-    res.status(500).send("Server Error");
-  }
-});
-
-// @route   Put api/posts/like/:id <- endpoint
-// @desc    Like a post
-// @access  Private (or private: You need to send a token to that route, in order for it to work.)
-
-router.put("/like/:id", auth, async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-
-    //Check if post has already been liked
-    if (
-      post.likes.filter((like) => like.user.toString() === req.user.id).length >
-      0
-    ) {
-      return res.status(400).json({ msg: "Post already liked" });
-    }
-
-    post.likes.unshift({ user: req.user.id });
-    await post.save();
-    res.json(post.likes);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
-});
-
-// @route   Put api/posts/unlike/:id <- endpoint
-// @desc    Unlike a post
-// @access  Private (or private: You need to send a token to that route, in order for it to work.)
-
-router.put("/unlike/:id", auth, async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-
-    //Check if post has already been liked
-    if (
-      post.likes.filter((like) => like.user.toString() === req.user.id)
-        .length === 0
-    ) {
-      return res.status(400).json({ msg: "Post not liked yet" });
-    }
-
-    // Get remove index
-    const removeIndex = post.likes
-      .map((like) => like.user.toString())
-      .indexOf(req.user.id);
-
-    post.likes.splice(removeIndex, 1);
-
-    await post.save();
-    res.json(post.likes);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send("Server Error");
-  }
-});
-
-// @route   POST api/posts/comment/:id <- endpoint
-// @desc    Comment on a post
-// @access  Private (or private: You need to send a token to that route, in order for it to work.)
-router.post(
-  "/comment/:id",
-  [auth, [check("text", "Text is required").not().isEmpty()]],
-  async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ erros: errors.array() });
-    }
-    try {
-      const user = await User.findById(req.user.id).select("-password");
-      const post = await Post.findById(req.params.id);
-
-      const newComment = {
-        text: req.body.text,
-        name: user.name,
-        avatar: user.avatar,
-        user: req.user.id,
-      };
-      post.comments.unshift(newComment);
-      await post.save();
-
-      res.json(post.comments);
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).send("Server Error");
-    }
-  }
-);
-
-// @route   DELETE api/posts/comment/:id/:comment_id <- endpoint
-// @desc    Delete a comment
-// @access  Private (or private: You need to send a token to that route, in order for it to work.)
-router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-
-    //Get the comment from the post
-    const comment = post.comments.find(
-      (comment) => comment.id === req.params.comment_id
-    );
-
-    //Make sure comment exists
-    if (!comment) {
-      return res.status(404).json({ msg: "Comment does not exist" });
-    }
-
-    //Check if user is owner of comment
-
-    if (comment.user.toString() !== req.user.id) {
-      return res.status(401).json({ msg: "User not authorized" });
-    }
-
-    const removeIndex = post.comments
-      .map((comment) => comment.user.toString())
-      .indexOf(req.user.id);
-
-    post.comments.splice(removeIndex, 1);
-
-    await post.save();
-
-    res.json(post.comments);
-  } catch (err) {
-    console.error(err.message);
     res.status(500).send("Server Error");
   }
 });
