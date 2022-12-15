9b999289585b1cfe21a9cfd02149fa4c3394e7bb
Yoshinori Takeshima <yotakeshima@gmail.com>
2022-12-15 17:46:25 -0500

    added frontend routes to register and login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,5 @@
 import React, { Fragment } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
@@ -15,14 +10,15 @@ import "./App.css";
 const App = () => (
   <Router>
     <Fragment>
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<Landing />} />
       </Routes>
-      <section className="containter">
-        <Switch>
+      <section className="container">
+        <Routes>
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/login" element={<Login />} />
-        </Switch>
+        </Routes>
       </section>
     </Fragment>
   </Router>
