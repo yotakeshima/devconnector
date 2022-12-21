e6dc42735f4b12547efb6a2b47ad7d67d504425b
Yoshinori Takeshima <yotakeshima@gmail.com>
2022-12-20 22:23:35 -0500

    finished adding the register inputs, but warnings still occur

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
-import "./App.css";
+import './App.css';
 
 const App = () => (
   <Router>
     <Fragment>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Landing />} />
+        <Route path='/' element={<Landing />} />
       </Routes>
-      <section className="container">
+      <section className='container'>
         <Routes>
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/login" element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </section>
     </Fragment>
