a72cee8a1d3318324c0bb35c93df8f54ee5e9bf0
Yoshinori Takeshima <yotakeshima@gmail.com>
2023-01-19 22:57:07 -0500

    added Alert layout and reducers

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,10 +1,14 @@
 import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux';
+// Connects redux and react
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
+// Every time an action is brought in, needs to be passed in through connect()
 // import axios from "axios";
 
-const Register = (props) => {
+// setAlert is destructured from props...
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -20,7 +24,7 @@ const Register = (props) => {
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      props.setAlert('Passwords do not match', 'danger');
+      setAlert('Passwords do not match', 'danger');
     } else {
       console.log('SUCCESS');
       // // ACCESSING THE BACKEND REGISTERING A USER EXAMPLE
@@ -106,6 +110,10 @@ const Register = (props) => {
       </p>
     </Fragment>
   );
+};
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
 };
 
 export default connect(null, { setAlert })(Register);
--- client/src/components/layout/ContainerLayout.js
+++ client/src/components/layout/ContainerLayout.js
@@ -1,9 +1,10 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Alert from './Alert';
 const ContainerLayout = () => {
   return (
-    <section className="container">
+    <section className='container'>
+      <Alert />
       <Outlet />
     </section>
   );
