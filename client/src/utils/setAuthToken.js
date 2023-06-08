f0b616f91253f5f1839080d3b3b10dc1367aa358
Yoshinori Takeshima <yotakeshima@gmail.com>
2023-06-08 16:08:42 -0400

    added comments to utils

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
-import e from 'express';
 
+// Creates a global header for axios
 const setAuthToken = (token) => {
   if (token) {
     axios.defaults.headers.common['x-auth-token'] = token;
   } else {
+    // if it is not a token, delete it from the global headers
     delete axios.defaults.headers.common['x-auth-token'];
   }
 };
