c248d77afdafe86b24b00e4bb9fe3a898c681086
Yoshinori Takeshima <yotakeshima@gmail.com>
2023-06-12 21:31:15 -0400

    added a spinner for loading screen. Added Links to Navbard to prevent a authenticated user to sign in again. Added CLEAR_PROFILE. Added a create profile button

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,6 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
-const Landing = () => {
+import React from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" />;
+  }
+
   return (
     <section className="landing">
       <div className="dark-overlay">
@@ -24,4 +31,12 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps)(Landing);
