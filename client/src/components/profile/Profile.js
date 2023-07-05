a30823f204cb2c9adc753c10172f8d733eee4f26
Yoshinori Takeshima <yotakeshima@gmail.com>
2023-07-05 13:17:08 -0400

    added githubrepo display and fixed errors. Profile education and experience proptype changed to object, and changed all classes to className

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
 import ProfileExperience from './ProfileExperience';
 import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
 import { getProfileById } from '../../actions/profile';
 import withRouter from '../utils/withRouter';
 
@@ -36,7 +37,7 @@ const Profile = ({
                 Edit Profile
               </Link>
             )}
-          <div class="profile-grid my-1">
+          <div className="profile-grid my-1">
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
             <div className="profile-exp bg-white p-2">
@@ -69,6 +70,9 @@ const Profile = ({
                 <h4>No education credentials</h4>
               )}
             </div>
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
           </div>
         </Fragment>
       )}
