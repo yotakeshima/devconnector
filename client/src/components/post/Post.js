b754ce7f4c9fd58086580d524b80e314eaa27fd6
Yoshinori Takeshima <yotakeshima@gmail.com>
2023-07-08 11:20:22 -0400

    created a CommentForm for people to comment on posts

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
 import { getPost } from '../../actions/post';
 import withRouter from '../utils/withRouter';
 
@@ -19,6 +20,7 @@ const Post = ({ getPost, post: { post, loading }, params }) => {
         Back To Posts
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
     </Fragment>
   );
 };
